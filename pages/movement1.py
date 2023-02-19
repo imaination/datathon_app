c7b6fe84d96266bd0f7ac48d169d1baeb8ea58a2
Ruriko Imai <ruriko.imai@student.kuleuven.be>
2023-02-19 10:24:35 +0100

    Create movement1.py

---
+++ pages/movement1.py
@@ -0,0 +1,17 @@
+import dash
+import dash_bootstrap_components as dbc
+
+dash.register_page(__name__, name="movement1", top_nav=True,)
+
+
+def layout():
+	carousel = dbc.Carousel(
+		items=[
+			{"key": "1", "src": "https://kuleuven-datathon-2023.s3.eu-central-1.amazonaws.com/images/Vincent+Van+Gogh/Wheatfield+with+Crows.jpg", "header": "HEADER HERE", "caption": "caption here, maybe generate images to text",},
+			{"key": "2", "src": "https://kuleuven-datathon-2023.s3.eu-central-1.amazonaws.com/images/Vincent+Van+Gogh/Cypress+against+a+Starry+Sky.jpg", "header": "HEADER HERE", "caption": "caption here, maybe generate images to text",},
+			{"key": "3", "src": "https://kuleuven-datathon-2023.s3.eu-central-1.amazonaws.com/images/Vincent+Van+Gogh/A+Marsh.jpg", "header": "HEADER HERE", "caption": "caption here, maybe generate images to text",},
+		],
+		className="carousel-fade",
+		style={"width": "80%", "height": "50%", "padding-top": "70px", "margin-left": "auto", "margin-right": "auto"}
+	)
+	return carousel
