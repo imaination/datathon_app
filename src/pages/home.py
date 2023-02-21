34626238379bd5a45d34dcc0701466ff1505225e
Ruriko Imai <ruriko.imai@student.kuleuven.be>
2023-02-21 20:41:18 +0100

    abled scrolling

--- src/pages/home.py
+++ src/pages/home.py
@@ -17,11 +17,11 @@ def generate_card(movement, caption, image_link, link):
                                         caption,
                                         className="card-text",
                                 ),
-                                dbc.Button(movement, external_link=False, href=link,  color="primary"),
+                                dbc.Button(movement, external_link=False, href=link, color="warning"),
                         ]
                 ),
         ],
-        style={"width": "18rem", "height": "100%"},
+        style={"width": "18rem", "height": "35rem", "overflow": "scroll"},
         )
         return card
 
@@ -35,7 +35,7 @@ def generate_card_list(df):
         movement = df.movement[row]
         description = df.description[row]
         img = df.image_url[row]
-        cards.append(dbc.Col(generate_card(movement, description, img, link='/{}'.format(movement)),width="auto"))
+        cards.append(dbc.Col(generate_card(movement, description, img, link='/{}'.format(movement)), style={"width":"auto"} ))
 
     return cards
 
@@ -45,18 +45,9 @@ DATA_PATH = os.path.join(PATH, "src/assets/unique_movements.csv")
 movements_df = pd.read_csv(DATA_PATH)
 
 cards_gen = dbc.Row(
-	generate_card_list(movements_df)
+	generate_card_list(movements_df),
 )
 
-#cards = dbc.Row(
-#        [
-#                dbc.Col(generate_card("Symbolism", "A late 19th-century art movement of French and Belgiam origin in poetry and other arts seeking to represent absolute truths symbolically through language and metaphorical images.", "https://kuleuven-datathon-2023.s3.eu-central-1.amazonaws.com/images/Pierre+Auguste+Renoir/Le+Pont+Neuf+Paris.jpg", link='/movement1'), width="auto"),
-#                dbc.Col(generate_card("Intimism", "An artistic movement in the late 19th-century and early 20th-century that involved the depiction of banal yet personal domestic scenes, particularly those within domestic interiors.", "https://kuleuven-datathon-2023.s3.eu-central-1.amazonaws.com/images/Vincent+Van+Gogh/Peach+Trees+in+Blossom.jpg", link="https://dash-bootstrap-components.opensource.faculty.ai/docs/components/card/#"), width="auto"),
-#	 ]
-#)
-
-
 layout = html.Div([
-	#cards,
 	cards_gen,
 ])
